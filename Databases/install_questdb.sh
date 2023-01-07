99fcf954220da36e486166da2af8bd4aed873a91
Abdel <abdel@diufmac12.unifr.ch>
2023-01-07 17:25:37 +0100

    clean

--- Databases/install_questdb.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/questdb/questdb/releases/download/6.2.1/questdb-6.2.1-rt-linux-amd64.tar.gz
-tar xvf questdb-6.2.1-rt-linux-amd64.tar.gz 
-cd questdb-6.2.1-rt-linux-amd64/bin
-./questdb start
