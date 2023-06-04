34d31bb2cbc1642e045808319ca15b857b6b36bf
AbdelouahabKhelifati <ca_khelifati@esi.dz>
2023-06-04 13:19:15 +0000

    scripts

---
+++ systems/load_all.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+# ClickHouse
+cd clickhouse
+sh ./load.sh
+cd ..
+
+# Druid
+cd druid
+sh ./load.sh
+cd ..
+
+# ExtremeDB
+cd extremedb
+source ./load.sh
+cd ..
+
+# Influx
+cd influx
+sh ./load.sh
+cd ..
+
+# MonetDB
+cd monetdb
+sh ./load.sh
+cd ..
+
+
+# QuestDB
+cd questdb
+sh ./load.sh
+cd ..
+
+# TimescaleDB
+cd  timescaledb
+sh ./load.sh
+cd ..
