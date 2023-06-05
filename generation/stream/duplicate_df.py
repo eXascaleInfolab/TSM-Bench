394f393d031d056c4430feda9a98fc852697caf2
AbdelouahabKhelifati <ca_khelifati@esi.dz>
2023-06-05 12:04:16 +0000

    delete gen

--- generation/stream/duplicate_df.py
+++
@@ -1,8 +0,0 @@
-
-import numpy as np
-from tqdm import tqdm
-data = np.loadtxt('/Users/abdel/PycharmProjects/tsgen/results/goldwind3072/fake0.csv', delimiter=',')
-
-for i in tqdm(range(10)):
-    np.savetxt('/Users/abdel/PycharmProjects/tsgen/results/goldwind3072/fake'+str(data.shape[0]) + '.csv', data, delimiter=',')
-    data = np.concatenate((data, data[:3072]))
