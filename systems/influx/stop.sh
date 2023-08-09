9178294a478fb89439990cbc945c91ca267e735a
root <root@diufrm110.unifr.ch>
2023-08-09 15:24:06 +0000

    commit

--- systems/influx/stop.sh
+++ systems/influx/stop.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 
+sudo killall --user influxdb
+sudo killall --user telegraf
 sudo kill -9 $(sudo lsof -t -i:8080)
 ps -ef | grep 'influx' | grep -v grep | awk '{print $2}' | sudo xargs -r kill -9
 
