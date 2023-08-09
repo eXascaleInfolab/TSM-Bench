684bf781ff62312b56996957a71fcd898a35055a
root <root@diufrm110.unifr.ch>
2023-08-09 09:30:37 +0000

    remove &

--- systems/druid/install.sh
+++ systems/druid/install.sh
@@ -14,7 +14,7 @@ current=$(pwd)
 
 sed 's?path_to_file?'`pwd`/../../datasets/d1.csv'?' load_template.json > load.json
 
-time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 &
+time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 
 
 du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d1
 
