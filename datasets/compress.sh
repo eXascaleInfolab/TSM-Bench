a5a74de8d1e4731e05d4ac068fe4fa69f34f9522
removedporn <86824510+removedporn@users.noreply.github.com>
2023-08-09 12:26:34 +0800

    Delete datasets directory

--- datasets/compress.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p splits
-tar -cvzf - *.csv | split -b 50m - "splits/datasets_splits."
--- datasets/utils/concat_generated.sh
+++
@@ -1,8 +0,0 @@
-foo="paste -d' ' "
-for i in `seq 0 99`;
-do
-foo="${foo} fake_long${i}.csv"
-done
-foo="${foo} > fake_long_complete.csv"
-echo $foo
-eval $foo
