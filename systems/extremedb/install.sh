0ef6fd5feaf275b17255ce05d7afaa2923f25f81
root <root@diufrm110.unifr.ch>
2023-08-10 08:50:10 +0000

    commit

--- systems/extremedb/install.sh
+++ systems/extremedb/install.sh
@@ -23,21 +23,8 @@ pip3 install --force-reinstall eXtremeDB/target/python/dist/exdb_mcobject-0.1.1-
 current="$(pwd)"
 
 export MCO_ROOT="$current"/systems/eXtremeDB/
-export MCO_LIBRARY_PATH="$current"/systems//eXtremeDB/target/bin.so
+export MCO_LIBRARY_PATH="$current"/systems/eXtremeDB/target/bin.so
 export LD_LIBRARY_PATH="$current"/systems/eXtremeDB/target/bin.so
-
-
-export MCO_ROOT=/home/abdel/TSM-Bench/systems/extremedb/eXtremeDB/
-export MCO_LIBRARY_PATH=/home/abdel/TSM-Bench/systems/extremedb/eXtremeDB/target/bin.so
-export LD_LIBRARY_PATH=/home/abdel/TSM-Bench/systems/extremedb/eXtremeDB/target/bin.so
-
-
-cd eXtremeDB/target/bin
-./xsql -b -c xsql.cfg -p 5001 -f create.sql;
-
-du -sh datapoints.dbs
-
-
 
 
 # export MCO_PYTHONAPILIB=/home/abdel/.local/lib/python3.8/site-packages/exdb_mcobject-0.1.1-py3.8-linux-x86_64.egg/exdb/libmcopythonapi.so
