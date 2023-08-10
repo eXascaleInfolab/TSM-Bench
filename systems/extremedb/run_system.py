cb9acc9d11626cb478370a9212a67fac8c5d09ca
althausLuca <43413156+althausLuca@users.noreply.github.com>
2023-08-10 15:27:32 +0200

    Update exetremdb/run_system.py
    removing the communicate keeps the process running in the background.

--- systems/extremedb/run_system.py
+++ systems/extremedb/run_system.py
@@ -20,12 +20,10 @@ import subprocess
 from subprocess import Popen, PIPE, STDOUT, DEVNULL # py3k
 
 process = Popen(['sh', 'variables.sh'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-stdout, stderr = process.communicate()
 
 process = Popen(['sh', 'launch.sh'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-stdout, stderr = process.communicate()
 
-process = Popen(['sleep', '3'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+process = Popen(['sleep', '10'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
 stdout, stderr = process.communicate()
 
 import exdb 
