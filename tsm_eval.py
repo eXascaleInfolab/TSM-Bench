eb79704af51ee3336be3f9339139c996405e77a1
root <root@diufrm110.unifr.ch>
2023-08-10 15:55:04 +0000

    commit

--- tsm_eval.py
+++ tsm_eval.py
@@ -7,11 +7,11 @@ parser = argparse.ArgumentParser(description = 'Script for running any eval')
 parser.add_argument('--systems', nargs = '+', type = str, help = 'Systems name', default = ['clickhouse','druid','influx','monetdb','questdb','timescaledb'])
 parser.add_argument('--datasets', nargs = '*', type = str, help = 'Dataset name', default = 'd1')
 parser.add_argument('--queries', nargs = '?', type = str, help = 'List of queries to run (Q1-Q7)', default = "q1 q2 q3 q4 q5 q6 q7")
-parser.add_argument('--nb_st', nargs = '?', type = int, help = 'Number of stations in the dataset', default = 10)
-parser.add_argument('--nb_s', nargs = '?', type = int, help = 'Number of sensors in the dataset', default = 100)
-parser.add_argument('--def_st', nargs = '?', type = int, help = 'Default number of queried stations', default = 1)
-parser.add_argument('--def_s', nargs = '?', type = int, help = 'Default number of queried sensors', default = 3)
-parser.add_argument('--def_range', nargs = '?', type = str, help = 'Query range', default = "1d")
+parser.add_argument('--n_st', nargs = '?', type = int, help = 'Number of stations in the dataset', default = 10)
+parser.add_argument('--n_s', nargs = '?', type = int, help = 'Number of sensors in the dataset', default = 100)
+parser.add_argument('--nb_st', nargs = '?', type = int, help = 'Default number of queried stations', default = 1)
+parser.add_argument('--nb_sr', nargs = '?', type = int, help = 'Default number of queried sensors', default = 3)
+parser.add_argument('--range', nargs = '?', type = str, help = 'Query range', default = "1d")
 parser.add_argument('--max_ts', nargs = '?', type = str, help = 'Maximum query timestamp', default = "2019-04-30T00:00:00")
 parser.add_argument('--min_ts', nargs = '?', type = str, help = 'Minimum query timestamp', default = "2019-04-01T00:00:00")
 parser.add_argument('--timeout', nargs = '?', type = str, help = 'Query execution timeout in seconds', default = 20)
@@ -21,10 +21,10 @@ args = parser.parse_args()
 
 try:
     index = 0
-    while index < len(args.def_range) and args.def_range[index].isdigit():
+    while index < len(args.range) and args.range[index].isdigit():
         index += 1
-    args.range = int(args.def_range[:index])
-    args.rangeUnit = args.def_range[index:]
+    args.rangeUnit = args.range[index:]
+    args.range = int(args.range[:index])
     assert args.rangeUnit.upper() in ['S','M','H','D','W']
 except:
     print("Input string does not conform to the expected format.", args.rangeUnit.upper())
@@ -60,8 +60,8 @@ for system in systems:
 	os.chdir(systemPath)
 	
 	toRun = ['python3', 'run_system.py', '--datasets', ' '.join(args.datasets)
-		, '--queries', str(args.queries), '--nb_st', str(args.nb_st)
-		, '--nb_s', str(args.nb_s), '--range', str(args.range), '--rangeUnit', str(args.rangeUnit), '--timeout', str(args.timeout)]
+		, '--queries', str(args.queries), '--nb_st', str(args.n_st)
+		, '--nb_s', str(args.n_s), '--range', str(args.range), '--rangeUnit', str(args.rangeUnit), '--timeout', str(args.timeout)]
 		
 	if len(args.additional_arguments) > 0:
 		toRun = toRun + args.additional_arguments.split(" ")
