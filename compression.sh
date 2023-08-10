9525069b988e58b42511074f0672fa18c0b31adb
root <root@diufrm110.unifr.ch>
2023-08-10 11:14:23 +0000

    commit

--- compression.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo docker start clickhouse-container
-
-sudo docker exec -it clickhouse-container clickhouse-client --query "SELECT table, formatReadableSize(sum(bytes)) as size FROM system.parts WHERE active AND table='d1' GROUP BY table;"
-
-sudo docker stop clickhouse-container
