46e09a02eeec940d6df1d46ebb58b64fc38f7444
Your Name <you@example.com>
2023-08-16 10:51:29 +0000

    Your commit message here

--- systems/clickhouse/run_system.py
+++ systems/clickhouse/run_system.py
@@ -15,15 +15,15 @@ from library import *
 
 print('launching system')
 
-# import os
-# import subprocess
-# from subprocess import Popen, PIPE, STDOUT, DEVNULL # py3k
+import os
+import subprocess
+from subprocess import Popen, PIPE, STDOUT, DEVNULL # py3k
 # 
-# process = Popen(['sh', 'launch.sh', '&'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-# stdout, stderr = process.communicate()
+process = Popen(['sh', 'launch.sh', '&'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+stdout, stderr = process.communicate()
 # 
-# process = Popen(['sleep', '2'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-# stdout, stderr = process.communicate()
+process = Popen(['sleep', '2'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+stdout, stderr = process.communicate()
 
 
 from clickhouse_driver import Client
