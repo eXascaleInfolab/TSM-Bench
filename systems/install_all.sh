b0be541d1cc7b8d1f802b926a1e76b6e3158e8a2
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-29 07:48:08 -0700

    Update install_all.sh

--- systems/install_all.sh
+++ systems/install_all.sh
@@ -3,35 +3,49 @@
 # ClickHouse
 cd clickhouse
 sh ./install.sh
+sh ./launch.sh
+sh ./setup.sh
 cd ..
 
 # Druid
-cd druid
-sh ./install.sh
-cd ..
+# cd druid
+# sh ./install.sh
+# sh ./launch.sh
+# sh ./setup.sh
+# cd ..
 
 # ExtremeDB
 cd extremedb
-source ./install.sh
+sh ./install.sh
+sh ./launch.sh
+sh ./setup.sh
 cd ..
 
 # Influx
-cd influx
-sh ./install.sh
-cd ..
+# cd influx
+# sh ./install.sh
+# sh ./launch.sh
+# sh ./setup.sh
+# cd ..
 
 # MonetDB
 cd monetdb
 sh ./install.sh
+sh ./launch.sh
+sh ./setup.sh
 cd ..
 
 
 # QuestDB
 cd questdb
 sh ./install.sh
+sh ./launch.sh
+sh ./setup.sh
 cd ..
 
 # TimescaleDB
 cd  timescaledb
 sh ./install.sh
+sh ./launch.sh
+sh ./setup.sh
 cd ..
