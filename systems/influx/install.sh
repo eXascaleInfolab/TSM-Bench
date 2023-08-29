273141eb59cb225360e73dc478116eb7d8b156d1
Your Name <you@example.com>
2023-08-29 07:59:00 +0000

    putting back stashed changes

--- systems/influx/install.sh
+++ systems/influx/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 sudo systemctl stop influxd
 
+
+pip3 install influxdb
+>>>>>>> 8ff33e4e34f9d09531ea520d1ef987048386d8e9
 wget https://dl.influxdata.com/influxdb/releases/influxdb-1.7.10_linux_amd64.tar.gz
 tar xvfz influxdb-1.7.10_linux_amd64.tar.gz
 rm influxdb-1.7.10_linux_amd64.tar.gz
