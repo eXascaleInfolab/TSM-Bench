8c826ac4ff3a5a3c13567d435134f955a40c12fc
althausluca <luca.althaus@students.unibe.ch>
2023-08-31 13:39:19 +0000

    plot modified, influx run made clearer

--- systems/questdb/install.sh
+++ systems/questdb/install.sh
@@ -8,6 +8,8 @@ wget https://github.com/questdb/questdb/releases/download/6.4.1/questdb-6.4.1-rt
 tar -xf questdb-6.4.1-rt-linux-amd64.tar.gz
 rm questdb-6.4.1-rt-linux-amd64.tar.gz
 
+sudo mkdir /root/.questdb/conf
+sudo mkdir $HOME/.questdb/conf/
 sudo cp server.conf $HOME/.questdb/conf/
 sudo cp server.conf /root/.questdb/conf/
 
