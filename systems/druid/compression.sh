60054010775151bf2bd8a0baec8635dc47799faf
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:25:29 -0700

    Update compression.sh

--- systems/druid/compression.sh
+++ systems/druid/compression.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
+
+
 du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d1
+#du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d2
