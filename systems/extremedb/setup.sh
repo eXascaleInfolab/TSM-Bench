582785ddcd760c810b22436ae7214eb3d7f7cd27
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:20:52 -0700

    Update setup.sh

--- systems/extremedb/setup.sh
+++ systems/extremedb/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
+
+
 cd eXtremeDB/target/bin
 ./xsql -b -c xsql.cfg -p 5001 -f create.sql;
 
 du -sh datapoints.dbs
+
+
+#./xsql -b -c xsql.cfg -p 5001 -f create2.sql;
+#du -sh datapoints.dbs
