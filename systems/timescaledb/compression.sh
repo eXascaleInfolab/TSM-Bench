0ca29393a700c3a80c4f7dfa4d6ef748560e72e8
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:26:59 -0700

    Update compression.sh

--- systems/timescaledb/compression.sh
+++ systems/timescaledb/compression.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# THE FOLLOWING SCRIPT WILL PRINT STORAGE SIZE FOR D1, TO SHOW D2 UNCOMMENT THE LINES BELOW 
+
 sudo docker start timescaledb-container
 
 sudo docker exec -it timescaledb-container psql -U postgres -c "SELECT hypertable_size('d1') ;";
+#sudo docker exec -it timescaledb-container psql -U postgres -c "SELECT hypertable_size('d2') ;";
 
 sudo docker stop timescaledb-container
