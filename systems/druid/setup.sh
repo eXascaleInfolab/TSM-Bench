caebd424e55ddd078abe989f51e1c6ae54561b9c
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:21:34 -0700

    Update setup.sh

--- systems/druid/setup.sh
+++ systems/druid/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
+
+
 current=$(pwd)
 
 
@@ -13,3 +16,8 @@ echo "load database"
 du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d1
 
 echo "DONE"
+
+
+#sed 's?path_to_file?'`pwd`/../../datasets/d2.csv'?' load_template.json > load.json
+#echo "load json"
+#time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 &
