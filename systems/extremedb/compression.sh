6dc50801dd5aff3bc4f97474a1bd1d538aac6672
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:26:32 -0700

    Update compression.sh

--- systems/extremedb/compression.sh
+++ systems/extremedb/compression.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# THE FOLLOWING SCRIPT WILL PRINT STORAGE SIZE FOR D1, TO SHOW D2 UNCOMMENT THE LINES BELOW 
+
 cd eXtremeDB/target/bin
 ./xsql -b -c xsql.cfg -p 5001 -f create.sql;
 
 du -sh datapoints.dbs
+#du -sh datapoints2.dbs
