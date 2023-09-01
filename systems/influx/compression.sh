7b1ec54df92fd08b7c4047dee20a209a643bdf94
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:26:42 -0700

    Update compression.sh

--- systems/influx/compression.sh
+++ systems/influx/compression.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# THE FOLLOWING SCRIPT WILL PRINT STORAGE SIZE FOR D1, TO SHOW D2 UNCOMMENT THE LINES BELOW 
+
 sudo du -sh /var/lib/influxdb/data/d1
+#sudo du -sh /var/lib/influxdb/data/d2
