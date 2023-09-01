0c0744afc4e93f4e25aacadb53900d4457f6cab4
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:20:09 -0700

    Update setup.sh

--- systems/influx/setup.sh
+++ systems/influx/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
+
+
 sudo ./influxdb-1.7.10-1/usr/bin/influx -import -path=../../datasets/d1-influxdb.csv -precision=ms #> /dev/null
 
 sudo du -sh /var/lib/influxdb/data/d1
+
+
+#sudo ./influxdb-1.7.10-1/usr/bin/influx -import -path=../../datasets/d2-influxdb.csv -precision=ms #> /dev/null
+#sudo du -sh /var/lib/influxdb/data/d2
 
 
 
