7f18389810d1869b42a8150a869bb510a78ffd06
Abdelouahab Khelifati <abdel@exascale.info>
2023-08-31 23:24:33 -0700

    Update compression.sh

--- systems/clickhouse/compression.sh
+++ systems/clickhouse/compression.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
+
 sudo docker start clickhouse-container
 
 sudo docker exec -it clickhouse-container clickhouse-client --query "SELECT table, formatReadableSize(sum(bytes)) as size FROM system.parts WHERE active AND table='d1' GROUP BY table;"
+#sudo docker exec -it clickhouse-container clickhouse-client --query "SELECT table, formatReadableSize(sum(bytes)) as size FROM system.parts WHERE active AND table='d2' GROUP BY table;"
 
 sudo docker stop clickhouse-container
