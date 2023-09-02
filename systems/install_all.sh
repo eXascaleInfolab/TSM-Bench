b23a565284e47836d2a9ae0d79aa2dc4ea2780e0
Abdelouahab Khelifati <abdel@exascale.info>
2023-09-01 22:59:16 -0700

    Update install_all.sh

--- systems/install_all.sh
+++ systems/install_all.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
 # TO LOAD D2: 
-# setup.sh will load d1 to the systems, to load d2, uncomment the respective lines in the ./setup.sh of each system
+# load.sh will load d1 to the systems, to load d2, uncomment the respective lines in the ./load.sh of each system
 
 
 # ClickHouse
 cd clickhouse
 sh ./install.sh
 sh ./launch.sh
-sh ./setup.sh
+sh ./load.sh
 cd ..
 
 # Druid
 #cd druid
 #sh ./install.sh
 #sh ./launch.sh
-#sh ./setup.sh
+#sh ./load.sh
 #cd ..
 
 # ExtremeDB
 cd extremedb
 sh ./install.sh
-sh ./setup.sh
+sh ./load.sh
 cd ..
 
 # Influx
 # cd influx
 # sh ./install.sh
 # sh ./launch.sh
-# sh ./setup.sh
+# sh ./load.sh
 # cd ..
 
 # MonetDB
 cd monetdb
 sh ./install.sh
 sh ./launch.sh
-sh ./setup.sh
+sh ./load.sh
 cd ..
 
 
@@ -43,13 +43,13 @@ cd ..
 cd questdb
 sh ./install.sh
 sh ./launch.sh
-sh ./setup.sh
+sh ./load.sh
 cd ..
 
 # TimescaleDB
 cd  timescaledb
 sh ./install.sh
-sh ./setup.sh
+sh ./load.sh
 cd ..
 
 
