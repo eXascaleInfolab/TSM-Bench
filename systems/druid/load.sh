3daedd9ac6f6ec82704df977281cc513ee37a63a
Your Name <you@example.com>
2023-09-06 15:44:27 +0000

    druid load and install updated

--- systems/druid/load.sh
+++ systems/druid/load.sh
@@ -3,12 +3,19 @@
 # THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
 
 
+
+
 current=$(pwd)
 
 
 echo "start!" 
 sed 's?path_to_file?'`pwd`/../../datasets/d1.csv'?' load_template.json > load.json
 echo "load json"
+
+
+sh launch.sh
+
+
 time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 &
 
 
@@ -16,7 +23,7 @@ echo "load database"
 du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d1
 
 
-echo "DONE"
+echo "data loading finished"
 
 ## uncoment the following for D2 ##
 ###################################
