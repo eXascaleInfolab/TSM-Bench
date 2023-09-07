d0464b7c1016978e21ea69f54eeae42830d177e7
althausluca <luca.althaus@students.unibe.ch>
2023-09-07 09:38:08 +0000

    druids installation reworked

--- systems/druid/install.sh
+++ systems/druid/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ps -ef | grep 'druid' | grep -v grep | awk '{print $2}' | xargs -r kill -9
-sleep 10
+
+echo " start Druid install"
 
 
 pip3 install pydruid
@@ -10,23 +11,28 @@ wget https://dlcdn.apache.org/druid/25.0.0/apache-druid-25.0.0-bin.tar.gz
 tar -xf apache-druid-25.0.0-bin.tar.gz
 rm apache-druid-25.0.0-bin.tar.gz
 
+
+echo "start Druid server"
+
 time ./apache-druid-25.0.0/bin/start-single-server-medium &
-sleep 20
+#sleep  30
+
 
 current=$(pwd)
 
 
-echo "start!" 
-sed 's?path_to_file?'`pwd`/../../datasets/d1.csv'?' load_template.json > load.json
-echo "load json"
-time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 &
+#echo "start!"
+#sed 's?path_to_file?'`pwd`/../../datasets/d1.csv'?' load_template.json > load.json
+#echo "load json"
+#time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 &
+
+#sleep 50
 
 
-echo "load database"
-du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d1
+
+
+# du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d1
 
 echo "DONE"
 
-
-
-ps -ef | grep 'druid' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+sh load.sh
--- systems/druid/load.sh
+++ systems/druid/load.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
-# THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
+# THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW
+
+
 
 
 current=$(pwd)
 
 
-echo "start!" 
+echo "start!"
 sed 's?path_to_file?'`pwd`/../../datasets/d1.csv'?' load_template.json > load.json
 echo "load json"
 
-sh launch.sh
 
+sh launch.sh
 
 sleep 600
 
-sh launch.sh && time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081
+time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 &
 
 
-#time ./apache-druid-25.0.0/bin/post-index-task --file load.json --url http://localhost:8081 &
+
 
 
 echo "load database"
 #du -sh ./apache-druid-25.0.0/var/druid/segment-cache/d1
 
 
-echo "DONE"
+echo "data loading finished"
 
 ## uncoment the following for D2 ##
 ###################################
