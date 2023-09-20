6622e958313cf0ee53e7d71e2689b154d5fe0eb6
althausLuca <43413156+althausLuca@users.noreply.github.com>
2023-09-20 09:38:22 +0000

    Update install_dependencies.sh

--- systems/install_dependencies.sh
+++ systems/install_dependencies.sh
@@ -1,111 +1,62 @@
 #!/bin/sh
-sudo apt update
 
+sudo apt-get update
+sudo apt-get install -y python3.8-venv
+sudo apt-get install -y libpython3.8-dev
+sudo apt-get install -y python3-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y unzip
+
+python3.8 -m venv venv
+. venv/bin/activate
+python3.8 -m pip install --upgrade pip
 # Python3.8 (the extremdb installation settup requires to build a 3.8 wheel)
-sudo apt install -y python3.8
-sudo apt install -y python3-pip
 
-pip3 install numpy
 pip3 install saxpy
-# pip3 install pandas
-# pip3 install protobuf==3.13.0
-# pip3 install tqdm
-# pip3 install gdown
-# pip3 install pylab-sdk
-# pip3 install sqlalchemy
-# pip3 install matplotlib
-# pip3 install matplotlib_terminal
+pip3 install numpy
+pip3 install pandas
+pip3 install protobuf==3.13.0
+pip3 install tqdm
+pip3 install gdown
+pip3 install pylab-sdk
+pip3 install sqlalchemy
+pip3 install matplotlib
+pip3 install matplotlib_terminal
 
-# apt-get install  libpq-dev -y
-# pip3 install psycopg2
-# pip3 install pymonetdb
+sudo apt-get install  libpq-dev -y
+pip3 install psycopg2
+pip3 install pymonetdb
 
-# # Java
-# sudo apt-get install -y openjdk-8-jdk
-# sudo update-java-alternatives --jre-headless --jre --set java-1.8.0-openjdk-amd64
+# Java
+sudo apt-get install -y openjdk-8-jdk
+sudo update-java-alternatives --jre-headless --jre --set java-1.8.0-openjdk-amd64
 
-# # Maven
-# sudo apt install -y maven
+# Maven
+sudo apt install -y maven
 
-# # Curl
-# sudo apt install -y curl
+# Curl
+sudo apt install -y curl
 
-# # Docker
-# sudo apt install -y docker.io
-# sudo systemctl start docker
-# sudo systemctl enable docker
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-# docker-compose --version
-
-
-# # Java
-# sudo apt-get install -y openjdk-8-jdk
-# sudo update-java-alternatives --jre-headless --jre --set java-1.8.0-openjdk-amd64
-
-# # Maven
-# sudo apt install -y maven
-
-# # Curl
-# sudo apt install -y curl
-
-# # Docker
-# sudo apt install -y docker.io
-# sudo systemctl start docker
-# sudo systemctl enable docker
+# Docker
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 
-# sudo apt update
+# Java
+sudo apt-get install -y openjdk-8-jdk
+sudo update-java-alternatives --jre-headless --jre --set java-1.8.0-openjdk-amd64
 
-# # Python3.8 (the extremdb installation settup requires to build a 3.8 wheel)
-# sudo apt install -y python3.8
-# sudo apt install -y python3-pip
-# sudo pip3 install saxpy
-# sudo pip3 install numpy
-# sudo pip3 install pandas
-# sudo pip3 install protobuf==3.13.0
-# sudo pip3 install tqdm
-# sudo pip3 install gdown
-# sudo pip3 install pylab-sdk
-# sudo pip3 install sqlalchemy
-# sudo pip3 install matplotlib
-# sudo pip3 install matplotlib_terminal
+# Maven
+sudo apt install -y maven
 
-# sudo apt-get install  libpq-dev -y
-# sudo pip3 install psycopg2
-# sudo pip3 install pymonetdb
+# Curl
+sudo apt install -y curl
 
-# # Java
-# sudo apt-get install -y openjdk-8-jdk
-# sudo update-java-alternatives --jre-headless --jre --set java-1.8.0-openjdk-amd64
-
-# # Maven
-# sudo apt install -y maven
-
-# # Curl
-# sudo apt install -y curl
-
-# # Docker
-# sudo apt install -y docker.io
-# sudo systemctl start docker
-# sudo systemctl enable docker
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-# docker-compose --version
-
-
-# # Java
-# sudo apt-get install -y openjdk-8-jdk
-# sudo update-java-alternatives --jre-headless --jre --set java-1.8.0-openjdk-amd64
-
-# # Maven
-# sudo apt install -y maven
-
-# # Curl
-# sudo apt install -y curl
-
-# # Docker
-# sudo apt install -y docker.io
-# sudo systemctl start docker
-# sudo systemctl enable docker
-
+# Docker
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
