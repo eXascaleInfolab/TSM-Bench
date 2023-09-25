76971207d2056f25a1ed8ff9bf045790a697f951
althausluca <althausluca>
2023-09-25 17:06:24 +0000

    influx updated

--- systems/influx/load.sh
+++ systems/influx/load.sh
@@ -2,8 +2,54 @@
 
 # THE FOLLOWING SCRIPT WILL SETUP AND LOAD D1, TO LOAD D2 UNCOMMENT THE LINES BELOW 
 
+log_file="influx_startup.txt"
+start_time=$(date +%s.%N)
 
-sudo ./influxdb-1.7.10-1/usr/bin/influx -import -path=../../datasets/d1-influxdb.csv -precision=ms #> /dev/null
+
+sudo ./influxdb-1.7.10-1/usr/bin/influx -import -path=../../datasets/d1-influxdb.csv -precision=ms > "$log_file" 2>&1 &
+
+echo "start loading"
+
+# Initialize the last_line variable
+last_line=0
+
+# Continuously monitor the log file for new lines
+while : ; do
+  # Get the current line count
+  current_lines=$(awk 'END {print NR}' "$log_file")
+
+  echo "$current_lines"
+
+  # Check if there are new lines since the last check
+  if [ "$current_lines" -gt "$last_line" ]; then
+    # Print the new lines
+    sed -n "$((last_line + 1)),$current_lines p" "$log_file"
+    echo "test"
+
+    # Update the last_line variable
+    last_line="$current_lines"
+
+    # Check if any line in the new lines contains "failed"
+    if grep -q "failed" "$log_file"; then
+      echo "Failed message detected! Exiting..."
+      break  # Exit the loop when "failed" is found
+    fi
+
+    if grep -q "loading complete!" "$log_file"; then
+      echo "loading complete! Exiting..."
+      break  # Exit the loop when "failed" is found
+    fi
+  fi
+
+  sleep 10  # Wait for a few seconds before checking again
+done
+rm $log_file
+
+end_time=$(date +%s.%N)
+elapsed_time=$(echo "$end_time - $start_time" | bc)
+echo "Loading time: $elapsed_time seconds" > loading_time.txt
+echo $elapsed_time
+
 
 sudo du -sh /var/lib/influxdb/data/d1
 
