6ab4043f3e94cd30ac13ca0778063ddc995db128
althausluca <althausluca>
2023-09-26 15:42:12 +0000

    system insall all updated

---
+++ systems/install_all_sys.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+cd systems
+was_in_root=$?
+
+# ClickHouse
+cd clickhouse
+sh ./install.sh
+cd ..
+
+# Druid
+cd druid
+sh ./install.sh
+sh ./launch.sh
+cd ..
+
+# ExtremeDB
+cd extremedb
+sh ./install.sh
+cd ..
+
+# MonetDB
+cd monetdb
+sh ./install.sh
+cd ..
+
+
+# QuestDB
+cd questdb
+sh install.sh
+cd ..
+
+cd extremedb
+sh ./install.sh
+cd ..
+
+cd questdb
+sh install.sh
+cd ..
+
+# TimescaleDB
+cd  timescaledb
+sh ./install.sh
+cd ..
+
+#Influx
+cd influx
+sh ./install.sh
+cd ..
+
+
+
+
+if [ $was_in_root -eq 0 ]; then
+    cd ..
+fi
