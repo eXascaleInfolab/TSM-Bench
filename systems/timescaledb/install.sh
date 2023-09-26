c8c57d2c81da1ca8f05e0cfba340f8ee3b6e4f74
Abdelouahab Khelifati <abdel@exascale.info>
2023-09-26 11:59:21 +1000

    Update install.sh

--- systems/timescaledb/install.sh
+++ systems/timescaledb/install.sh
@@ -1,32 +1,24 @@
 #!/bin/sh
 
 pip3 install psycopg2-binary
-
 sudo docker pull timescale/timescaledb-ha:pg14-latest
-
 sudo docker stop timescaledb-container
 sudo docker rm timescaledb-container
 
-
 ABSOLUTE_PATH=$(readlink -f "../../../")
-
 sudo docker run -d --name timescaledb-container \
-	-p 127.0.0.1:5431:5432 \
+	-p 5432:5432 \
 	-v $ABSOLUTE_PATH:/var/lib/postgresql/data \
 	-e POSTGRES_PASSWORD=postgres \
 	timescale/timescaledb-ha:pg14-latest
 
-
-#  docker exec -it timescaledb psql -U postgres
-#https://docs.timescale.com/install/latest/installation-docker/
-
-
 sudo docker start timescaledb-container
-#sudo docker exec -it timescaledb-container psql -U postgres
+sudo docker cp ./pg_hba.conf  timescaledb-container:/home/postgres/pgdata/data/
+sudo docker restart timescaledb-container
 
 sleep 25
-
-
 sh load.sh
 
-
+#sudo docker exec -it timescaledb-container psql -U postgres
+#  docker exec -it timescaledb psql -U postgres
+#https://docs.timescale.com/install/latest/installation-docker/
