368838e936890b9aa355a3a916a358b36fcad743
althausLuca <43413156+althausLuca@users.noreply.github.com>
2023-09-27 13:05:20 +0000

    Create start_template.py

---
+++ systems/integration/start.py
@@ -0,0 +1,14 @@
+def launch():
+	print('launching monetdb')
+
+	import os
+	import subprocess
+	from subprocess import Popen, PIPE, STDOUT, DEVNULL # py3k
+  
+  ## Replace this with the same code as the launch section in the run_system.py file
+  # Example:
+	#process = Popen(['sh', 'launch.sh', '&'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+	#stdout, stderr = process.communicate()
+
+	#process = Popen(['sleep', '2'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+	#stdout, stderr = process.communicate()
