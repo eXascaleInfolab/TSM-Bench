51ad8f576bee4a2799447930ed0c3f8e3a208e46
AbdelouahabKhelifati <ca_khelifati@esi.dz>
2023-09-29 02:09:05 +0000

    d1

--- datasets/build_d1.sh
+++ datasets/build_d1.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-mkdir -p splits 
-cat d1_splits/datasets_splits.* > datasets.tar.gz
-tar -zxvf datasets.tar.gz
-mv datasets/* ./
-rm datasets -r
-rm datasets.tar.gz
+#mkdir -p splits 
+#cat d1_splits/datasets_splits.* > datasets.tar.gz
+#tar -zxvf datasets.tar.gz
+#mv datasets/* ./
+#rm datasets -r
+#rm datasets.tar.gz
+
+wget -O d1.csv https://zenodo.org/record/8385578/files/d1.csv?download=1
 
 python3 generate_influx.py
 
 
 
-head -n 1 d1.csv > d1_tail.csv && tail -n 1 d1.csv >> d1_tail.csv
+# head -n 1 d1.csv > d1_tail.csv && tail -n 1 d1.csv >> d1_tail.csv
