0ba5ab96081a561b0df3caddf734e0fc15cc656a
Abdelouahab Khelifati <abdel@exascale.info>
2023-10-01 21:58:37 +1000

    Update build_d2.sh

--- datasets/build_d2.sh
+++ datasets/build_d2.sh
@@ -2,9 +2,9 @@
 
 
 wget -O d2_1_4.csv https://zenodo.org/record/8393992/files/d2_aa?download=1
-wget -O d2_2_4.csv 
-wget -O d2_3_4.csv 
-wget -O d2_4_4.csv 
+wget -O d2_2_4.csv https://zenodo.org/record/8394000/files/d2_ab?download=1
+wget -O d2_3_4.csv https://zenodo.org/record/8394002/files/d2_ac?download=1
+wget -O d2_4_4.csv https://zenodo.org/record/8394004/files/d2_ad?download=1
 
 cat d2_1_4.csv d2_2_4.csv d2_3_4.csv d2_4_4.csv > d2.csv
 
