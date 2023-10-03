eaae446ace5d447f1239d2491630bb0cafdcc1d8
AbdelouahabKhelifati <abdel@exascale.info>
2023-10-03 06:56:59 +0000

    moved load all

---
+++ datasets/load_all.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+dataset="d1"
+if [ $# -ge 1 ]; then
+    dataset="$1"
+fi
+echo "loading $dataset"
+
+cd ../systems
+
+# TimescaleDB
+cd  timescaledb
+sh ./load.sh $dataset
+cd ..
+
+# ClickHouse
+cd clickhouse
+sh ./load.sh $dataset
+cd ..
+
+# ExtremeDB
+cd extremedb
+sh ./load.sh $dataset
+cd ..
+
+
+# MonetDB
+cd monetdb
+sh ./load.sh $dataset
+cd ..
+
+
+# QuestDB
+cd questdb
+sh ./load.sh $dataset
+cd ..
+
+sh stop_all.sh
+
+
+# TimescaleDB
+cd  timescaledb
+sh ./load.sh $dataset
+cd ..
+
+
+# Influx
+cd influx
+sh ./load.sh $dataset
+cd ..
+
+sh stop_all.sh
+
+# Druid
+cd druid
+sh ./load.sh $dataset
+cd ..
