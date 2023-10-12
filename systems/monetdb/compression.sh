105282dc1db4b39f64254bea6115becfaba85140
althausluca <althausluca>
2023-10-12 13:36:53 +0000

    removed sudo from monetdb compression and updated datageneration to not read in the whole dataset

--- systems/monetdb/compression.sh
+++ systems/monetdb/compression.sh
@@ -3,6 +3,6 @@
 # THE FOLLOWING SCRIPT WILL PRINT STORAGE SIZE FOR D1, TO SHOW D2 UNCOMMENT THE LINES BELOW 
 
 current="$(pwd)"
-results=$(sudo du -sk ./master_db/mydb)
+results=$( du -sk ./master_db/mydb)
 result=$(echo "$results" | awk '{print $1}')
-echo "$result"
+echo "${result}k"
--- systems/utils/online_library.py
+++ systems/utils/online_library.py
@@ -2,18 +2,19 @@
 
 import os
 import pandas as pd
-def generate_continuing_data(batch_size = 100000):
-	path_to_data_folder = "../datasets" if os.path.isdir("../datasets") else "datasets"
-	data_set_name = "d1_tail.csv" 
-	data_set_path =  f"{path_to_data_folder}/{data_set_name}"
+def generate_continuing_data(batch_size = 100000,dataset):
+    path_to_data_folder = "../datasets" if os.path.isdir("../datasets") else "datasets"
+    data_set_name = "d1_tail.csv" 
+    data_set_path =  f"{path_to_data_folder}/{data_set_name}"
+    tail_process = subprocess.Popen(['tail', data_set_path, '-n', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = tail_process.communicate()
+    date , station , *sensors = stdout.decode("utf-8").strip().split(",")
+    date_pd = pd.to_datetime(date)
 
-	date , station , *sensors = pd.read_csv(data_set_path).values[-1,:]
-	date_pd = pd.to_datetime(date)
-
-	new_time_stamps = [ pd.to_datetime(date) + pd.Timedelta(seconds=10 * i) for i in range(1,batch_size+1)]
-	print(new_time_stamps[0],new_time_stamps[-1])
-	data_to_insert = f"{new_time_stamps[0].strftime('%Y-%m-%dT%H:%M:%S')},{station},{ ','.join([str(e) for e in sensors])}"
-	return { "time_stamps" : [   t_s.strftime('%Y-%m-%dT%H:%M:%S') for t_s in new_time_stamps ],
-		 "stations" : ["st<st_id>"] * len(new_time_stamps),
-		 "sensors" : [sensors]* len(new_time_stamps) #sensor data
-		}
+    new_time_stamps = [ pd.to_datetime(date) + pd.Timedelta(seconds=10 * i) for i in range(1,batch_size+1)]
+    print(new_time_stamps[0],new_time_stamps[-1])
+    data_to_insert = f"{new_time_stamps[0].strftime('%Y-%m-%dT%H:%M:%S')},{station},{ ','.join([str(e) for e in sensors])}"
+    return { "time_stamps" : [   t_s.strftime('%Y-%m-%dT%H:%M:%S') for t_s in new_time_stamps ],
+         "stations" : ["st<st_id>"] * len(new_time_stamps),
+         "sensors" : [sensors]* len(new_time_stamps) #sensor data
+          }
