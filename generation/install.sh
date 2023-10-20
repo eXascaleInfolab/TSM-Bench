ba0ecfba80bf2817407f0d71162bda0ffe76a989
Abdelouahab Khelifati <abdel@exascale.info>
2023-10-20 14:05:53 +1000

    Update install.sh

--- generation/install.sh
+++ generation/install.sh
@@ -33,5 +33,4 @@ python -m pip install torchvision
 python -m pip install wfdb
 python -m pip install lshashpy3
 python -m pip  install opencv-python 
-python -m pip opencv-python-headless
 # Run: time python main_tsgen.py
