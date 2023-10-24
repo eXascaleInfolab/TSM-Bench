e0af1e414ecf45e8f2e7eaa0151c83ae8e3dc0cb
althausluca <althausluca>
2023-10-24 08:14:50 +0000

    clickhouse q5 added

--- datasets/build_d2.sh
+++ datasets/build_d2.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# Download the files with muted output
-wget -O d2_1_4.csv https://zenodo.org/record/8393992/files/d2_aa?download=1 &
-wget -O d2_2_4.csv https://zenodo.org/record/8394000/files/d2_ab?download=1 > /dev/null 2>&1 &
-wget -O d2_3_4.csv https://zenodo.org/record/8394002/files/d2_ac?download=1 > /dev/null 2>&1 &
-wget -O d2_4_4.csv https://zenodo.org/record/8394004/files/d2_ad?download=1 > /dev/null 2>&1 &
 
-wait
+wget -O d2_1_4.csv https://zenodo.org/record/8393992/files/d2_aa?download=1
+wget -O d2_2_4.csv https://zenodo.org/record/8394000/files/d2_ab?download=1
+wget -O d2_3_4.csv https://zenodo.org/record/8394002/files/d2_ac?download=1
+wget -O d2_4_4.csv https://zenodo.org/record/8394004/files/d2_ad?download=1
 
-# Concatenate the downloaded files
-cat d2_1_4.csv d2_2_4.csv d2_3_4.csv d2_4_4.csv > d2.csv
+(head -n -1 d2_1_4.csv; head -n -1 d2_2_4.csv; head -n -1 d2_3_4.csv; head -n -1 d2_4_4.csv) > d2.csv
 
-# Remove individual files
-#rm d2_*
+rm d2_*
--- systems/clickhouse/run_system.py
+++ systems/clickhouse/run_system.py
@@ -24,62 +24,71 @@ set_s = [str(random.randint(0,99)) for i in range(500)]
 set_date = [random.random() for i in range(500)]
 
 def run_query(query, rangeL ,rangeUnit ,n_st ,n_s ,n_it , host="localhost"):
-	# Connect to the system
-	conn = connect_ClickHouse(f"clickhouse://{host}",port=9000)
-	cursor = conn.cursor()#cursor = Client(host='localhost',port=9001)
-	runtimes = []
-	full_time = time.time()
-	for it in tqdm(range(n_it)):
-		date = random_date("2019-04-30T00:00:00", "2019-04-01T00:00:00" , set_date[(int(rangeL)*it)%500], dform = '%Y-%m-%dT%H:%M:%S')
-		temp = query.replace("<timestamp>", date)
-		temp = temp.replace("<range>", str(rangeL))
-		temp = temp.replace("<rangesUnit>", rangeUnit)
-		
-		# stations
-		li = ['st' + str(z) for z in random.sample(range(10), n_st)]
-		q = "(" + "'" + li[0] + "'"
-		for j in li[1:]:
-			q += ', ' + "'" + j + "'"
-		q += ")"
+    # Connect to the system
+    conn = connect_ClickHouse(f"clickhouse://{host}",port=9000)
+    cursor = conn.cursor()#cursor = Client(host='localhost',port=9001)
+    runtimes = []
+    full_time = time.time()
+    for it in tqdm(range(n_it)):
+        date = random_date("2019-04-30T00:00:00", "2019-04-01T00:00:00" , set_date[(int(rangeL)*it)%500], dform = '%Y-%m-%dT%H:%M:%S')
+        temp = query.replace("<timestamp>", date)
+        temp = temp.replace("<range>", str(rangeL))
+        temp = temp.replace("<rangesUnit>", rangeUnit)
 
-		q_ = "("+', '.join(["'"+j+"'" for j in li ])+")"
-		assert q_ == q
+        # sensors
+        li = ['s' + str(z) for z in random.sample(range(100), n_s)]
+        q = li[0]
+        q_filter = '(' + li[0] + ' > 0.95' + ')'
+        q_avg = 'avg(' + li[0] + ')'
+        for j in li[1:]:
+            q += ', ' + j
+            # q_filter += ' OR ' + j + ' > 0.95'
+            q_avg += ', ' + 'avg(' + j + ')'
 
-		temp = temp.replace("<stid>", q)
-	
-		# sensors
-		li = ['s' + str(z) for z in random.sample(range(100), n_s)]
-		q = li[0]
-		q_filter = '(' + li[0] + ' > 0.95' + ')'
-		q_avg = 'avg(' + li[0] + ')'
-		for j in li[1:]:
-			q += ', ' + j
-			# q_filter += ' OR ' + j + ' > 0.95'
-			q_avg += ', ' + 'avg(' + j + ')'
+        temp = temp.replace("<sid>", q)
+        temp = temp.replace("<sfilter>", q_filter)
+        temp = temp.replace("<avg_s>", q_avg)
+        temp = temp.replace("<sid1>", "1")
+        temp = temp.replace("<sid2>", "2")
+        
+        # stations with special interpolation case
+        if "fill step" in temp.lower():
+            if n_st == 1:
+                q = "('"+'st'+str(random.sample(range(10), 1)[0])+"')"
+                temp = temp.replace("<stid>", q)
 
-		temp = temp.replace("<sid>", q)
-		temp = temp.replace("<sfilter>", q_filter)
-		temp = temp.replace("<avg_s>", q_avg)
-		temp = temp.replace("<sid1>", "1")
-		temp = temp.replace("<sid2>", "2")		
-		start = time.time()
+            else:
+                station_list = ['st' + str(z) for z in random.sample(range(10), n_st)] # sample stations
+                fill_commands = [] # queries to unite
+                for station in station_list:
+                    q = f"('{station}')"
+                    station_fill = temp.replace("<stid>", q).replace(";","")
+                    fill_commands.append(station_fill)
+                
+                temp = "SELECT * FROM (" + " UNION ALL ".join(fill_commands) + ")"
+        
+        else: # normal station insertion
+            station_list = ['st' + str(z) for z in random.sample(range(10), n_st)] # sample stations
+            q = "("+', '.join(["'"+j+"'" for j in station_list ])+")"
+            temp = temp.replace("<stid>", q)
+            
+            
+        start = time.time()
 
-		import re
-		pattern = r"<\S+>"
-		matches = re.findall(pattern, temp)
-		#assert not matches , temp
-		print(temp)
+        
+        #print(temp)
+        #time.sleep(15)
+        cursor.execute(temp)
+        cursor.fetchall()
+        #print(cursor.execute(temp))
+        #print(cursor.fetchall())
+        diff = (time.time()-start)*1000
+        runtimes.append(diff)
+        if time.time() - full_time > 200 and it > 5:
+            break  
 
-		print(cursor.execute(temp))
-		#results_ = cursor.fetchall()
-		diff = (time.time()-start)*1000
-		#  print(temp, diff)
-		runtimes.append(diff)
-		if time.time() - full_time > 200 and it > 5: 
-			break  
-			
-	#conn.close()
-	return stats.mean(runtimes), stats.stdev(runtimes)
+    conn.close()
+    return stats.mean(runtimes), stats.stdev(runtimes)
 
 
 
@@ -87,21 +96,21 @@ def run_query(query, rangeL ,rangeUnit ,n_st ,n_s ,n_it , host="localhost"):
 
 
 if __name__ == "__main__":
-	import os
-	import subprocess
-	from subprocess import Popen, PIPE, STDOUT, DEVNULL # py3k
+    import os
+    import subprocess
+    from subprocess import Popen, PIPE, STDOUT, DEVNULL # py3k
 
-	process = Popen(['sh', 'launch.sh'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-	stdout, stderr = process.communicate()
-	process = Popen(['sleep', '10'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-	stdout, stderr = process.communicate()
-	
-	args = init_parser() 
-	
-	def query_f(query, rangeL = args.range, rangeUnit = args.rangeUnit, n_st = args.def_st, n_s = args.def_s, n_it = args.n_it , host="localhost"):
-		return run_query(query, rangeL=rangeL, rangeUnit = rangeUnit ,n_st = n_st , n_s = n_s , n_it = n_it,host=host)
-	
-	run_system(args,"clickhouse",query_f)
+    process = Popen(['sh', 'launch.sh'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+    stdout, stderr = process.communicate()
+    process = Popen(['sleep', '10'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+    stdout, stderr = process.communicate()
+    
+    args = init_parser() 
+    
+    def query_f(query, rangeL = args.range, rangeUnit = args.rangeUnit, n_st = args.def_st, n_s = args.def_s, n_it = args.n_it , host="localhost"):
+        return run_query(query, rangeL=rangeL, rangeUnit = rangeUnit ,n_st = n_st , n_s = n_s , n_it = n_it,host=host)
+    
+    run_system(args,"clickhouse",query_f)
 
-	process = Popen(['sh', 'stop.sh'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-	stdout, stderr = process.communicate()
+    process = Popen(['sh', 'stop.sh'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+    stdout, stderr = process.communicate()
