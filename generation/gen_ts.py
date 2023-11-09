807a0bbda90f2f64255eba829412bc3525b4a7c3
AbdelouahabKhelifati <abdel@exascale.info>
2023-11-09 14:31:28 +1000

    control the number/length of the time series

--- generation/gen_ts.py
+++ generation/gen_ts.py
@@ -1,6 +1,6 @@
 import json
 import multiprocessing
-import lsh_main as lsh
+import hashing.lsh_main as lsh
 import time
 from pathlib import Path
 import pandas as pd
@@ -18,11 +18,11 @@ import argparse
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
-def moving_avg(x, n):
+def moving_avg(x, len_ts):
     cumsum = np.cumsum(np.insert(x, 0, 0)) 
-    return (cumsum[n:] - cumsum[:-n]) / float(n)
+    return (cumsum[len_ts:] - cumsum[:-len_ts]) / float(len_ts)
 
-def TS_LSH(data, segments, m, n):
+def TS_LSH(data, segments, nb_ts, len_ts):
     print('Building LSH...')
     lsh = lshash.LSHash(8, window, num_hashtables=8)
 
@@ -35,8 +35,8 @@ def TS_LSH(data, segments, m, n):
     n_top = 10
     k = int(window * .03)
 
-    n = n if n > len(to_query) else len(to_query)
-    for i in tqdm(range(m)):
+    len_ts = len_ts if len_ts > len(to_query) else len(to_query)
+    for i in tqdm(range(nb_ts)):
         for _ in range(5):
             try:
                 temp = list(random.choice( lsh.query(to_query[0], distance_func="euclidean", num_results=n_top))[0][0])
@@ -45,7 +45,7 @@ def TS_LSH(data, segments, m, n):
                 pass
         else:
             temp = list(to_query[0] + np.random.normal(0,.008, window))
-        for i in range(1, n // window + 2): 
+        for i in range(1, len_ts // window + 2): 
             for _ in range(5):
                 try:
                     s = list(random.choice( lsh.query(to_query[i%len(to_query)], distance_func="euclidean", num_results=n_top))[0][0])
@@ -61,33 +61,33 @@ def TS_LSH(data, segments, m, n):
                 overlap.append((1 - sigmoid(i))*temp_t[i + k//2] + sigmoid(i)*s_h[i + k//2])
             temp[-k:] = overlap
             temp.extend(s[k:])
-        lsh_res.append(temp[:n])    
+        lsh_res.append(temp[:len_ts])    
     return lsh_res
 
 def filter_segment(df_segments):
     print('Filtering GAN generated data...')
-    n = 10  # the larger n is, the smoother curve will be
-    b = [1.0 / n] * n
+    len_ts = 10  # the larger len_ts is, the smoother curve will be
+    b = [1.0 / len_ts] * len_ts
     a = 1
     for col in tqdm(df_segments):
         yy = lfilter(b,a,df_segments[col].tolist())
-        yy[:n] = yy[n:n+1]
+        yy[:len_ts] = yy[len_ts:len_ts+1]
         df_segments[col] = yy.tolist()
     df_segments.iloc[: , :50].plot(subplots=True, layout=(10,6), figsize=(10, 10), legend = True, color = 'b')
 #     plt.show()
     
-def plot_result(data, lsh_res, m, n):
+def plot_result(data, lsh_res, nb_ts, len_ts):
     print('Plotting the results')
-    xi = list(range(n))
+    xi = list(range(len_ts))
     plt.figure(figsize=(60, 30))
-    plt.subplot(m+1, 1, 1)
+    plt.subplot(nb_ts+1, 1, 1)
     # plt.figure(figsize=(60, 8))
-    plt.plot(data[:n],color='blue',linewidth=4.0, label='Original')
-    plt.xlim(0,n)
+    plt.plot(data[:len_ts],color='blue',linewidth=4.0, label='Original')
+    plt.xlim(0,len_ts)
 #     plt.ylim(8.1,8.7)
     plt.legend(loc="upper left", prop={'size': 36})
-    for i in range(2, m+2):
-        plt.subplot(m+1, 1, i)
+    for i in range(2, nb_ts+2):
+        plt.subplot(nb_ts+1, 1, i)
         plt.plot(lsh_res[i-2],color='green',linewidth=4.0, label='LSH')
         plt.xlim(0,len(lsh_res[0]))
 #         plt.ylim(8.1,8.7)
@@ -96,14 +96,14 @@ def plot_result(data, lsh_res, m, n):
 
 
 parser = argparse.ArgumentParser(description="A script that takes two integer values as input and calls a function with them.")
-parser.add_argument("--n", type=int, default=10000, help="Length of ts")
-parser.add_argument("--m", type=int, default=3, help="Number of ts")
+parser.add_argument("--len_ts", type=int, default=10000, help="Length of ts")
+parser.add_argument("--nb_ts", type=int, default=3, help="Number of ts")
 parser.add_argument("--fori", type=str, default='data/sample_dataset.csv', help="Link to original dataset")
 parser.add_argument("--fsynth", type=str, default='data/column_23_3072_3072.txt', help="Link to synthetic segments")
 parser.add_argument("--output_to", type=str, default='results/lsh.csv', help="Link to result file")
 args = parser.parse_args()
-n = args.n
-m = args.m
+len_ts = args.len_ts
+nb_ts = args.nb_ts
 fori = args.fori
 fsynth = args.fsynth
 output_to = args.output_to
@@ -127,9 +127,9 @@ df_segments = pd.read_csv(fsynth).T
 # # plt.show()
 # # df_segments = filter_segment(df_segments)
 
-lsh_res = TS_LSH(data, df_segments, m, n)
+lsh_res = TS_LSH(data, df_segments, nb_ts, len_ts)
 
-plot_result(data, lsh_res, m, n)
+plot_result(data, lsh_res, nb_ts, len_ts)
 lsh_res = pd.DataFrame(lsh_res).T
 lsh_res.to_csv(output_to, header = False, index = False, float_format='%.6f')
 
