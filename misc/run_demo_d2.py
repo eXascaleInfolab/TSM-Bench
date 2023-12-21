ee4641dd6272547c368955378b4894bf761a8ab3
ig <luca.althaus@students.unibe.ch>
2023-12-21 12:16:22 +0100

    online updated to changes in run systems

---
+++ misc/run_demo_d2.py
@@ -0,0 +1,92 @@
+import datetime
+
+import pandas
+import os
+
+import sys
+
+sys.path.append(os.getcwd())
+
+from utils.system_modules import system_module_map
+from utils.query_template_loader import load_query_tempaltes
+import argparse
+
+parser = argparse.ArgumentParser(description='Script for running any eval')
+
+parser.add_argument('--system', nargs="?",
+                    type=lambda x: str(x) if str(x) in system_module_map.keys()
+                    else exit(f"system {x} not valid must be one of {list(system_module_map.keys())}"),
+                    help='system name', required=True)
+
+parser.add_argument('--it', nargs="?", type=int, help='n_iterationss', default=100)
+
+args = parser.parse_args()
+
+system = args.system
+print(system)
+
+dataset = "d2"
+
+result_path = f"utils/full_results/{dataset}"
+os.makedirs(result_path, exist_ok=True)
+output_file = f"{result_path}/{system}.csv"
+log_file = f"{result_path}/{system}_log.csv"
+
+
+if os.path.exists(output_file):
+    pass
+else:
+    with open(output_file, "w") as file:
+        file.write( f"runtime, var  , query , n_s , n_st , timerange \n")
+
+with open(log_file, "w") as file:
+    file.write("")
+
+n_iter = args.it
+timeout = 1500
+n_sensors = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+n_stations = [1, 2, 3, ]
+time_ranges = ["minute", "hour", "day", "week", "month"]
+
+scenarios = [(sensor, station, time_range) for sensor in n_sensors for station in n_stations for time_range in
+             time_ranges]
+
+from systems import timescaledb
+system_module : timescaledb =  system_module_map[system]
+
+query_templates = load_query_tempaltes(system)
+
+already_computed_results = set()
+with open(output_file, "r") as file:
+    for line in file.readlines()[1:]:
+        if line == "":
+            continue
+        r_ , v_ , q , n_s , n_st , time_range = line.split(",")
+        already_computed_results.add( (q.strip(),int(n_s),int(n_st),time_range.strip()))
+
+print(already_computed_results)
+
+try:
+    system_module.launch()
+    for i, query in enumerate(query_templates):
+        if "select" not in query.lower():
+            continue
+        query = query.replace("<db>", dataset)
+        for n_s, n_st, time_range in scenarios:
+            if (f"q{i + 1}",n_s,n_st,time_range) in already_computed_results:
+                print("already computed")
+                continue
+            try:
+                time, var = system_module.run_query(query, rangeUnit=time_range, rangeL=1, n_s=n_s, n_it=n_iter, n_st=n_st,
+                                                    dataset=dataset)
+                with open(output_file, "a") as file:
+                    line = f"{time} , {var} , q{i + 1} , {n_s} , {n_st} , {time_range}\n"
+                    file.write(line)
+            except Exception as E:
+                with open(log_file, "a") as file:
+                    line = f"{E}"
+                    file.write(line)
+                print(E)
+
+finally:
+    system_module.stop()
