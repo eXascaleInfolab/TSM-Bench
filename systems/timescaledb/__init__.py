0857e94c3147081d0d17574c3bb41a396ed877ed
ig <luca.althaus@students.unibe.ch>
2024-01-04 17:32:13 +0100

    mongodb query 1 and 2 work without projection

--- systems/timescaledb/__init__.py
+++ systems/timescaledb/__init__.py
@@ -8,5 +8,5 @@ os.chdir(dname)
 
 
 from systems.timescaledb.run_system import run_query , launch , stop , get_connection
-from systems.timescaledb.add_data import input_data , delete_data , generate_insertion_query
+from systems.timescaledb.add_data import, delete_data , generate_insertion_query
 os.chdir(curr_dir)
