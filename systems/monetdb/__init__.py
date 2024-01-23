094bf2fc1518cede7414d2aa05a774eb40e42462
ig <luca.althaus@students.unibe.ch>
2024-01-23 15:14:48 +0100

    parse query added to init

--- systems/monetdb/__init__.py
+++ systems/monetdb/__init__.py
@@ -6,6 +6,6 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-from systems.monetdb.run_system import run_query , launch , stop , get_connection
+from systems.monetdb.run_system import run_query , launch , stop , get_connection , parse_query
 from systems.monetdb.add_data import delete_data , generate_insertion_query
 os.chdir(curr_dir)
