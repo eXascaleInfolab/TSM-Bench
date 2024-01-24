5bc30b7892bce7a8af09da996b3a17eca2de8269
ig <luca.althaus@students.unibe.ch>
2024-01-24 11:16:34 +0100

    parse query added to init

--- misc/run_demo_online.py
+++ misc/run_demo_online.py
@@ -69,8 +69,8 @@ n_rows = [int(batch_size / 100 / n_threads)]  # *100 for the batch size * 10 for
 #  quest db does not support multi threading for insertion
 if system == "questdb":
     print("questdb does not support multi threading for insertion setting number of threads to 1")
-    n_threads = 1
     n_rows = [i * n_threads for i in n_rows]
+    n_threads = 1
     assert n_rows[0] == batch_size / 100 , ( batch_size , n_rows[0] , n_threads )
 
 system_module: timescaledb = system_module_map[system]
