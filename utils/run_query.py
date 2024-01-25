429588274915033915b32fc3b809a602228fe37d
ig <luca.althaus@students.unibe.ch>
2024-01-25 09:53:01 +0100

    parse query added to init

--- utils/run_query.py
+++ utils/run_query.py
@@ -7,10 +7,13 @@ from tqdm import tqdm
 
 def run_query(system_module, query_template,  rangeL, rangeUnit, n_st, n_s, n_it, dataset, host="localhost",log=True):
     ## specify the dataset in the query templae the query tempalte
-    query_template = query_template.replace("<db>", dataset)
+    query_template = query_template.replace("<db>", dataset) # replace the dataset name in the query template
+
     # Connect to the system
     client : Connection = system_module.get_connection(host=host,dataset=dataset)
 
+
+    # get the randomized inputs
     random_inputs = get_randomized_inputs(dataset, n_st=n_st, n_s=n_s, rangeL=rangeL, n_it=n_it)
     random_stations = random_inputs["stations"]
     random_sensors = random_inputs["sensors"]
@@ -19,7 +22,7 @@ def run_query(system_module, query_template,  rangeL, rangeUnit, n_st, n_s, n_it
     runtimes = []
     full_time = time.time()
 
-    for it in tqdm(range(n_it)):
+    for it in tqdm(range(n_it+2)):
         date = random_sensors_dates[it]
         sensor_list = random_sensors[it]
         station_list = random_stations[it]
@@ -30,6 +33,10 @@ def run_query(system_module, query_template,  rangeL, rangeUnit, n_st, n_s, n_it
 
         query = system_module.parse_query(query_template, date=date, rangeUnit=rangeUnit, rangeL=rangeL, sensor_list=sensor_list,
                             station_list=station_list)
+
+        if it < 1: # 2 warmup iterations
+            continue
+
         if log and it==0:
             print("query")
             print(query)
