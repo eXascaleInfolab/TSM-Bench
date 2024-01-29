79e76118ef3fdbdcf3a31ec95356eeeff0809c98
ig <luca.althaus@students.unibe.ch>
2024-01-29 09:44:57 +0100

    commit featured datasets

--- systems/timescaledb/generate_compression.sh
+++ systems/timescaledb/generate_compression.sh
@@ -12,6 +12,10 @@ for scarsity in 10 30 60 80; do
     sh load.sh ${file_name}_scarsity_$scarsity
 done
 
+for outliers in 1 2 3 10; do
+    echo "outliers: $outliers"
+    sh load.sh ${file_name}_outliers_$outliers
+done
 
 for repeats in 10 30 70 90; do
     echo "repeats: repeats"
