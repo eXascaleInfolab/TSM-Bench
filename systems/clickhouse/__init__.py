fdf9fc92b1eb68c1a9a1228174c73208d7c2e669
ig <luca.althaus@students.unibe.ch>
2024-01-31 13:48:53 +0100

    run systems update

--- systems/clickhouse/__init__.py
+++ systems/clickhouse/__init__.py
@@ -1,12 +1,3 @@
-import os
-
-curr_dir = os.getcwd()
-
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-    
 from systems.clickhouse.run_system import *
-from systems.clickhouse.online_utils import input_data , delete_data , generate_insertion_query
+from systems.clickhouse.online_utils import  delete_data , generate_insertion_query
 
-os.chdir(curr_dir)
