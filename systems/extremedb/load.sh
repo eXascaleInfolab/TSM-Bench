aea25d438bbad75651193bbe07be0e7ac5fd9793
Abdelouahab Khelifati <abdel@exascale.info>
2024-04-25 17:22:30 +0200

    Update load.sh

--- systems/extremedb/load.sh
+++ systems/extremedb/load.sh
@@ -9,7 +9,7 @@ fi
 echo "loading $dataset into extremedb"
 
 current="$(pwd)"
-. variables.sh
+# . variables.sh
 export MCO_ROOT="$current"
 export MCO_LIBRARY_PATH="$current"/eXtremeDB/target/bin.so
 export LD_LIBRARY_PATH="$current"/eXtremeDB/target/bin.so
