8c751212a920fe650a64fb763ca66e74d1ef1eab
Abdelouahab Khelifati <abdel@exascale.info>
2024-05-02 17:46:32 +0200

    Update launch.sh

--- systems/influx/launch.sh
+++ systems/influx/launch.sh
@@ -5,7 +5,7 @@ log_file="influxdb.log"
 
 # "" > "$log_file"
 
-./influxdb-1.7.10-1/usr/bin/influxd > "$log_file" 2>&1 &
+sudo ./influxdb-1.7.10-1/usr/bin/influxd > "$log_file" 2>&1 &
 
 sleep 10
 echo "influx launched"
